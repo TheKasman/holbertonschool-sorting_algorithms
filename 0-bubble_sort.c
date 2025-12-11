00fccbbf999d1e7c9017184419970174e63d78da
TheKasman <kasapidisandrew@gmail.com>
2025-12-11 13:56:30 +1100

    the loop became undefined

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 	for (outer = 0; outer < size - 1; outer++)
 	{
 	/*loop 2 - look at each element and swap spots if a is smaller than b */
-		for (inner = 0; inner <= size - outer - 1; inner++)
+		for (inner = 0; inner < size - outer - 1; inner++)
 		{
 			if (array[inner] > array[inner + 1])
 			{
