443358bd22fffb1071b48d369b2935ce101f6fdf
TheKasman <kasapidisandrew@gmail.com>
2025-12-12 17:37:30 +1100

    restructure complete

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,41 @@
 #include "sort.h"
 
 /**
+ * partition - helper function for quick sort to create partitions
+ * @arr: the array
+ * @n: array size
+ * @orig: the original bounds of the array
+ * @osize: the original size of the array
+ * Return: returns
+ */
+
+static size_t partition(int *arr, size_t n, int *orig, size_t osize)
+{
+	int pivot, temp;
+	size_t i = 0, j;
+
+	pivot = arr[n - 1];
+
+	for (j = 0; j < n - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			print_array(orig, osize);
+		}
+	}
+	temp = arr[i];
+	arr[i] = arr[n - 1];
+	arr[n - 1] = temp;
+	print_array(orig, osize);
+	return (i);
+}
+
+
+/**
  * quick_sort - uses quick sort (splits the array up using a pivot and go)
  * @array: the array
  * @size: the sizeof the array
@@ -8,38 +43,22 @@
 
 void quick_sort(int *array, size_t size)
 {
-	int pivot, temp;
-	size_t i, j, left, right;
-	static int *original_array = NULL;
-	static size_t original_size = 0;
+	size_t pivot;
+	static int *original;
+	static size_t orig_size;
 
-	if (!original_array)
+	if (!original)
 	{
-		original_array = array;
-		original_size = size;
+		original = array;
+		orig_size = size;
 	}
 
 	/*base case!*/
 	if (size <= 1)
 		return;
 
-	pivot = array[size / 2];
+	pivot = partition(array, size, original, orig_size);
 
-	i = 0;
-	for (j = 0; j < size; j++)
-	{
-		if (array[j] < pivot)
-		{
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			i++;
-			print_array(original_array, original_size);
-		}
-	}
-	left = i;
-	right = size - i;
-
-	quick_sort(array, left);
-	quick_sort(array + i, right);
+	quick_sort(array, pivot);
+	quick_sort(array + pivot + 1, size - pivot - 1);
 }
